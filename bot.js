aada12566ae6746b5b08054d92cc85a08ad60b56
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-14 00:21:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,47 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "#"// البرفكس هنا
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('By Diamond Codes')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-
-  
-  
-  
- 
-  
-  
-  
-  
-  
-  });
-  
-  
- 
- 
- const Discord = require('discord.js');
-const client = new Discord.Client();
 const UserBlocked = new Set(); 
 const settings = require('./settings.json')
 ,ti={}  
